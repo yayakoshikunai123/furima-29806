119bdf5c3035759df46931a25b4d556121bad7ca
yayakoshikunai123 <unvipquality@yahoo.co.jp>
2020-09-21 14:58:43 +0900

    初めてのデプロイ

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_001708) do
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "image"
-    t.string "text"
-    t.text "message"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
