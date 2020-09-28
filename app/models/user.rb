3c3cd09960549e0ca95bfe36d711ddadc6a2cadc
yayakoshikunai123 <unvipquality@yahoo.co.jp>
2020-09-28 18:17:25 +0900

    ユーザー管理機能、新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  with_options presence: true do  
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+    # validates :encrypted_password, format: { with: /\A[a-z0-9]+\z/i, message: "は半角英数で入力してください。"}
+  end
 end
