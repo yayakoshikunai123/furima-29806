a39ff26cd0e0d23b843eac7bf577bba17620860f
yayakoshikunai123 <unvipquality@yahoo.co.jp>
2020-10-04 15:47:07 +0900

    商品出品登録　３回目（利益実装済み）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,29 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
+  belongs_to_active_hash :category                
   belongs_to_active_hash :product_status
   belongs_to_active_hash :shipping_day
   belongs_to_active_hash :shipping_fee_burden
   belongs_to_active_hash :shipping_source_area
   belongs_to_active_hash :product_status
 
-  validates :image, :expanation, :price, :product_name, :category, :product_status, :shipping_day, :shipping_fee_burden, :shipping_source_area, presence: true 
-           
+  with_options presence: true do
+    validates :image              
+    validates :product_name
+    validates :expanation
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than: 10_000_000 }
+  end
+
   has_one_attached :image
 
   belongs_to :user
 
   with_options numericality:   { other_than: 1 } do
-    validates :category_id, :product_status_id, :shipping_day_id, :shipping_fee_burden_id, :shipping_source_area_id, :product_status_id
+    validates :category_id 
+    validates :product_status_id
+    validates :shipping_day_id
+    validates :shipping_fee_burden_id
+    validates :shipping_source_area_id
+    validates :product_status_id
   end
 end
