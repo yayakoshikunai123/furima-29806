9bd6bdc90ce0bf315e882986fe64c6962489e38c
yayakoshikunai123 <unvipquality@yahoo.co.jp>
2020-10-15 00:27:52 +0900

    デプロイ後　修正1回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,8 +26,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if @item.valid?
-       @item.update(item_params)
+    if @item.update(item_params)
        redirect_to root_path
     else
       render :edit
